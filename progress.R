0780bfa6ff6ec91418e9623383f4b56b71dded57
Trevor Tombe <trevor.tombe@gmail.com>
2021-12-15 16:50:43 -0700

    update

--- progress.R
+++ progress.R
@@ -17,18 +17,22 @@ accessed<-paste0(accessed_date," at ",accessed_time," ET")
 # Progress Bar
 latest_data<-as.data.frame(fromJSON('https://api.covid19tracker.ca/summary'))
 df<-data.frame(
-  type=c("first","second"),
+  type=c("first","second","third"),
   value=c((as.numeric(latest_data$data.total_vaccinations)-
-             as.numeric(latest_data$data.total_vaccinated))/38434169, # latest real-time estimate from https://www150.statcan.gc.ca/n1/pub/71-607-x/71-607-x2018005-eng.htm
-          as.numeric(latest_data$data.total_vaccinated)/38434169)
+             as.numeric(latest_data$data.total_vaccinated)-
+             as.numeric(latest_data$data.total_boosters_1))/38434169,
+          as.numeric(latest_data$data.total_vaccinated)/38434169, # latest real-time estimate from https://www150.statcan.gc.ca/n1/pub/71-607-x/71-607-x2018005-eng.htm
+          as.numeric(latest_data$data.total_boosters_1)/38434169)
 )
 ggplot(df,aes(type,value,fill=type))+
   geom_col(aes(y=1),fill='gray90',width=0.5)+
   geom_col(width=0.5)+
   annotate('text',x=1.4,y=0,size=5,label=paste("At least one dose:  "),hjust=0)+
-  annotate('text',x=2.4,y=0,size=5,label=paste("Fully vaccinated (% of Total Pop.):  "),hjust=0)+
+  annotate('text',x=2.4,y=0,size=5,label=paste("Fully vaccinated:  "),hjust=0)+
+  annotate('text',x=3.4,y=0,size=5,label=paste("Boosted (% of Total Pop.):  "),hjust=0)+
   annotate('text',x=1.4,y=1,size=5,label=paste0(round(100*df[1,2],1),"%"),hjust=1)+
   annotate('text',x=2.4,y=1,size=5,label=paste0(round(100*df[2,2],1),"%"),hjust=1)+
+  annotate('text',x=3.4,y=1,size=5,label=paste0(round(100*df[3,2],1),"%"),hjust=1)+
   coord_flip()+
   theme_void()+
   theme(panel.grid.major.y = element_blank(),
@@ -37,7 +41,7 @@ ggplot(df,aes(type,value,fill=type))+
         axis.text.y = element_blank(),
         axis.text.x = element_blank(),
         legend.position = 'none')+
-  scale_fill_manual(values=c('dodgerblue','#CC2529'),'gray')+
+  scale_fill_manual(values=c('#396ab1','#CC2529','#3E9651'),'gray')+
   scale_y_continuous(limit=c(0,1))+
   labs(x="",y="")
 ggsave('Plots/progress.png',width=5.5,height=3,dpi=400)
